54dbb1dbc3691bcbf44f03e0513dc987e76e18f4
AdrianGerman <adrian.german1019@gmail.com>
2024-10-04 12:16:38 -0600

    separate the data connection into a dependency

---
+++ src/main/java/com/example/demo/dbconfig/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.dbconfig;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.example.demo.mapper")
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring-task");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
+}
